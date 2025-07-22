17af7f5388eb95661a001fa4b914968c50a327ca
Tin0709 <2002trungtin@gmail.com>
2025-07-22 13:36:24 +0700

    web21 adding all needed files

---
+++ web21/rpsls-game/src/data/choices.js
@@ -0,0 +1,9 @@
+const choices = {
+  rock: { name: "Rock", beats: ["scissors", "lizard"] },
+  paper: { name: "Paper", beats: ["rock", "spock"] },
+  scissors: { name: "Scissors", beats: ["paper", "lizard"] },
+  lizard: { name: "Lizard", beats: ["spock", "paper"] },
+  spock: { name: "Spock", beats: ["scissors", "rock"] },
+};
+
+export default choices;
