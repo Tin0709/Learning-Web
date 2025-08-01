b93bd8efe33e82e638514fbb86e8674b0ab6af53
Tin0709 <2002trungtin@gmail.com>
2025-08-01 14:21:55 +0700

    web32 completed

--- web32/book-library-manager/src/pages/Home.jsx
+++ web32/book-library-manager/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import BookList from "../components/BookList";
+import { useBooks } from "../context/BookContext";
 
 export default function Home() {
+  const { books } = useBooks();
+
   return (
     <div className="container">
       <h2>Your Book Library</h2>
-      <BookList />
+      {books.length === 0 ? (
+        <div className="empty-state">
+          No books yet. Click <strong>"Add Book"</strong> to get started!
+        </div>
+      ) : (
+        <BookList />
+      )}
     </div>
   );
 }
