34c496e13a6cea3e8cfb77d243f7576d40aa04b1
Tin0709 <2002trungtin@gmail.com>
2025-08-03 23:25:43 +0700

    web34 adding dark mode toggle

--- web34/script.js
+++ web34/script.js
@@ -47,3 +47,18 @@ form.addEventListener("submit", (e) => {
 });
 
 renderEntries();
+
+const darkToggle = document.getElementById("toggle-dark");
+const isDark = localStorage.getItem("darkMode") === "true";
+
+if (isDark) {
+  document.body.classList.add("dark-mode");
+}
+
+darkToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "darkMode",
+    document.body.classList.contains("dark-mode")
+  );
+});
